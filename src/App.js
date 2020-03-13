d2383278d775c2989581fa41da8c6bce5305b786
Tayfun Sur <tayfunsur@gmail.com>
2020-03-13 15:04:36 +0300

    fetching api function is written

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { 
+      data: {},
+      city: '' 
+    };
+    
+  }
+
+    
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.getApi();    
+  }
+
+  changeHandler = (e) => {
+    this.setState({city: e.target.value})
+  }
+
+  getApi = async() => {
+    const city = this.state.city;
+    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=05a4c77487211944ceff0c266384ab5b', {mode: 'cors'});
+    const tempData = await response.json();
+    this.setState({data: tempData})
+    console.log(this.state.data)
+    console.log(this.state.data.name)
+    console.log(this.state.data.visibility)
+    console.log(this.state.data.main.temp)    
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">       
+          <form onSubmit={this.handleSubmit}>
+            <input 
+              type="text" 
+              onChange={this.changeHandler}
+            />
+            <button type="submit">Search</button>
+          </form>
+
+        </header>
+      </div>
+    );
+  } 
+}
+
+function RenderResult(props) {
+  return(
+    <div>
+      {props.city}
+      {props.tempC}
     </div>
-  );
+  )
 }
 
 export default App;
