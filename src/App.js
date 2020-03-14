0e9db51d6a96bb3631e5b12e5cdeecb53766ee2b
Tayfun Sur <tayfunsur@gmail.com>
2020-03-14 09:26:50 +0300

    info display code is written

--- src/App.js
+++ src/App.js
@@ -6,37 +6,42 @@ class App extends Component {
     super();
     this.state = { 
       data: {},
-      city: '' 
+      city: '',
+      tempC: '',
+      tempF: '' 
     };
-    
+  }
+  getApi = async() => {
+    const city = this.state.city;
+    const apiKey = '05a4c77487211944ceff0c266384ab5b';
+    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid='+apiKey, {mode: 'cors'});
+    const tempData = await response.json();
+    this.setState({data: tempData}); 
+    this.convertToCelcius();  
+    this.convertToFahrenheit();
   }
 
-    
   handleSubmit = (e) => {
     e.preventDefault();
-    this.getApi();    
+    this.getApi();
   }
+
 
   changeHandler = (e) => {
     this.setState({city: e.target.value})
   }
 
-  getApi = async() => {
-    const city = this.state.city;
-    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=05a4c77487211944ceff0c266384ab5b', {mode: 'cors'});
-    const tempData = await response.json();
-    this.setState({data: tempData}) 
-    this.convertToCelcius();  
-    this.convertToFahrenheit();
-  }
-  
+
   convertToCelcius = () => {
-    console.log(Math.round((this.state.data.main.temp - 273.15)*100)/100)
+    this.setState({tempC: Math.round((this.state.data.main.temp - 273.15)*100)/100 })
   }
 
+
   convertToFahrenheit = () => {
-    console.log(Math.round((this.state.data.main.temp*9/5-459.67)*100)/100)
+    this.setState({tempF: Math.round((this.state.data.main.temp*9/5-459.67)*100)/100})
   }
+
+
   render() {
     return (
       <div className="App">
@@ -48,18 +53,33 @@ class App extends Component {
             />
             <button type="submit">Search</button>
           </form>
-
+          <div className="Result">
+            <RenderResult 
+              city={this.state.data.name}
+              tempC={this.state.tempC}
+              tempF={this.state.tempF}/>
+          </div>
         </header>
+        
       </div>
     );
   } 
 }
 
+
 function RenderResult(props) {
   return(
     <div>
-      {props.city}
-      {props.tempC}
+      <div>
+        {props.city}
+      </div>
+      
+      <div>
+        {props.tempC}
+      </div>
+      <div>
+        {props.tempF}
+      </div>
     </div>
   )
 }
