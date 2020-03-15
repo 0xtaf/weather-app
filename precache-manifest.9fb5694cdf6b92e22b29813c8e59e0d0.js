ca3f534f58e35c7c4a3d0d1e81bf857cb670e875
Tayfun Sur <tayfunsur@gmail.com>
2020-03-15 08:34:08 +0300

    Updates

---
+++ precache-manifest.9fb5694cdf6b92e22b29813c8e59e0d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d13c440c1fcc05d676a3227970759da9",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "5dc82a670ebc2fd3936f",
+    "url": "/weather-app/static/css/main.e883f94f.chunk.css"
+  },
+  {
+    "revision": "24c5f8a626bf20745c49",
+    "url": "/weather-app/static/js/2.ab1a8852.chunk.js"
+  },
+  {
+    "revision": "e2eae84c7a1d31e04c786e0eb21cdeb1",
+    "url": "/weather-app/static/js/2.ab1a8852.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dc82a670ebc2fd3936f",
+    "url": "/weather-app/static/js/main.d2f6941f.chunk.js"
+  },
+  {
+    "revision": "7385e57a5db20e0b1e05",
+    "url": "/weather-app/static/js/runtime-main.ca9a71b7.js"
+  }
+]);
